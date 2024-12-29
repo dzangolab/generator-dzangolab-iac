c5f30929007b760ec29fa5c8310d443d51e38e56
Anthony Phothirath <anthony.phothirath@hotmail.com>
2024-12-30 00:31:57 +0100

    chore: added prompt for do-swarm and option on generator

--- generators/ansible-do/index.js
+++ generators/ansible-do/index.js
@@ -2,9 +2,34 @@ import Generator from "yeoman-generator";
 
 export default class AnsibleDOGenerator extends Generator {
   writing() {
+    this.option("domain", {
+      type: String,
+      required: true,
+      default: "DOMAIN",
+      desc: "domain name."
+    });
+
+    this.option("deploy_group", {
+      type: String,
+      required: true,
+      default: "USERNAME",
+      desc: "deploy_group name."
+    });
+
+    this.option("deploy_user", {
+      type: String,
+      required: true,
+      default: "USERNAME",
+      desc: "deploy_user name."
+    });
+
     this.fs.copyTplAsync(
       this.templatePath("ansible/stack"),
       this.destinationPath(`ansible/${this.options.environment}`),
+      {
+        ...this.props,
+        ...this.options,
+      },
       { globOptions: { dot: true } }  
     );
   }

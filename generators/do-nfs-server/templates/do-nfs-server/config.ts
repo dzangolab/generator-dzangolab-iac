94f424daf73f2356e91003e15abce3a14cc4d73c
Olivier Pichon <203745+opichon@users.noreply.github.com>
2025-02-02 22:52:10 +0700

    Do nfs server (#40)
    * fix(do/nfs-server): fix firewall droplets
    
    * feat(do/nfs-server): add support for packages

--- generators/do-nfs-server/templates/do-nfs-server/config.ts
+++ generators/do-nfs-server/templates/do-nfs-server/config.ts
@@ -12,6 +12,8 @@ export const getConfig = async () => {
   const stack = getStack();
   const stackConfig = new Config();
 
+  const packages = stackConfig.getObject<string[]>("packages") || [];
+
   const pathToSshKeysFolder = stackConfig.get("pathToSshKeysFolder") || "../../ssh-keys";
 
   const publicKeyNames = stackConfig.requireObject("publicKeyNames") as string[];
@@ -80,6 +82,7 @@ export const getConfig = async () => {
   return {
     image: stackConfig.require("image"),
     name: stackConfig.get("name") || stack,
+    packages,
     pathToSshKeysFolder,
     projectId,
     protect: stackConfig.getBoolean("protect"),

76e8362d5bcfaf8656f92da71c6d5bd8dfb182f1
Olivier Pichon <op@dzango.com>
2025-02-23 23:46:55 +0800

    fix(pulumi-s3-backend): add support for passphrase secrets provider

--- generators/pulumi-s3-backend/templates/pulumi-s3-backend/config.ts
+++ generators/pulumi-s3-backend/templates/pulumi-s3-backend/config.ts
@@ -11,6 +11,7 @@ export const getConfig = async () => {
     name: stackConfig.get("name") || [getOrganization(), "pulumi-state"].join("-"),
     protect: stackConfig.getBoolean("protect"),
     retainOnDelete: stackConfig.getBoolean("retainOnDelete"),
+    secretsProvider: stackConfig.get("secretsProvider") || "passphrase"
   };
 };
 
--- generators/pulumi-s3-backend/templates/pulumi-s3-backend/index.ts
+++ generators/pulumi-s3-backend/templates/pulumi-s3-backend/index.ts
@@ -19,6 +19,8 @@ export = async () => {
     retainOnDelete: config.retainOnDelete,
   };
 
+  const outputs: { [key: string]: any } = {};
+
   const name = config.name;
 
   const bucket = new BucketV2(
@@ -67,7 +69,7 @@ export = async () => {
   );
 
   // Filter out null values and ensure a clean array
-  const accountArns = config.awsAccountArns.filter((arn) => arn !== null && arn !== undefined);
+  const accountArns = config.awsAccountArns.filter((arn: string) => arn !== null && arn !== undefined);
 
   if (accountArns.length > 0){
     // Create a bucket policy allowing access from multiple accounts
@@ -104,50 +106,62 @@ export = async () => {
     );
   }
 
-  const awsAccountId = (await getCallerIdentity()).accountId
+  outputs["bucketArn"] = interpolate`${bucket.arn}`;
+  outputs["bucketId"] = interpolate`${bucket.id}`;
+  outputs["pulumiBackendLoginCommand"] = interpolate`pulumi login s3://${bucket.id}`;
+  outputs["pulumiBackendUrl"] = interpolate`s3://${bucket.id}`;
 
-  const secretsEncryptionKey = new Key(
-    name,
-    {
-      description: "Key use to encrypt secrets",
-      deletionWindowInDays: config.keyDeletionWindow,
-      policy: JSON.stringify(
+  const secretsProvider = config.secretsProvider;
+
+  switch (secretsProvider) {
+    case "passphrase":
+      outputs["pulumiStackInitCommand"] = "pulumi stack init --secrets-provider=passphrase";
+      break;
+
+    case "kms": 
+      const awsAccountId = (await getCallerIdentity()).accountId
+
+      const secretsEncryptionKey = new Key(
+        name,
         {
-          Version: "2012-10-17",
-          Statement: [
-            // policy which gives the AWS account that owns the KMS key full access to the KMS key
+          description: "Key use to encrypt secrets",
+          deletionWindowInDays: config.keyDeletionWindow,
+          policy: JSON.stringify(
             {
-              Sid: "Enable IAM policies",
-              Effect: "Allow",
-              Action: "kms:*",
-              Principal: {
-                AWS: [`arn:aws:iam::${awsAccountId}:root`]
-              },
-              Resource: "*",
+              Version: "2012-10-17",
+              Statement: [
+                // policy which gives the AWS account that owns the KMS key full access to the KMS key
+                {
+                  Sid: "Enable IAM policies",
+                  Effect: "Allow",
+                  Action: "kms:*",
+                  Principal: {
+                    AWS: [`arn:aws:iam::${awsAccountId}:root`]
+                  },
+                  Resource: "*",
+                },
+              ],
             },
-          ],
+          ),
         },
-      ),
-    },
-    options,
-  );
+        options,
+      );
 
-  const alias = new Alias(
-    name,
-    {
-      name: `alias/${name}`,
-      targetKeyId: secretsEncryptionKey.keyId,
-    },
-    options,
-  );
+      const alias = new Alias(
+        name,
+        {
+          name: `alias/${name}`,
+          targetKeyId: secretsEncryptionKey.keyId,
+        },
+        options,
+      );
 
-  return {
-    bucketArn: interpolate`${bucket.arn}`,
-    bucketId: interpolate`${bucket.id}`,
-    pulumiBackendLoginCommand: interpolate`pulumi login s3://${bucket.id}`,
-    pulumiBackendUrl: interpolate`s3://${bucket.id}`,
-    pulumiSecretsProviderKeyId: interpolate`awskms:///${secretsEncryptionKey.keyId}`,
-    pulumiSecretsProviderKeyAlias: interpolate`awskms:///${alias.name}`,
-    pulumiStackInitCommand: interpolate`pulumi stack init --secrets-provider='awskms:///${secretsEncryptionKey.keyId}' <project_name>.<stack_name>`,
-  };
+      outputs["pulumiSecretsProviderKeyId"] = interpolate`awskms:///${secretsEncryptionKey.keyId}`;
+      outputs["pulumiSecretsProviderKeyAlias"] = interpolate`awskms:///${alias.name}`;
+      outputs["pulumiStackInitCommand"] = interpolate`pulumi stack init --secrets-provider='awskms:///${secretsEncryptionKey.keyId}' <project_name>.<stack_name>`;
+  
+      break;
+  }
+
+  return outputs;
 };

d990b98de58cd97689b6ae7d4d4c3e3bb26d5f1a
Anthony Phothirath <anthony.phothirath@hotmail.com>
2025-03-25 17:09:55 +0100

    chore: changed default value

--- generators/aws-swarm-leader/index.js
+++ generators/aws-swarm-leader/index.js
@@ -24,7 +24,7 @@ export default class AWSSwarmLeaderGenerator extends PulumiGenerator {
 
     this.option("size", {
       type: String,
-      default: "t3.small",
+      default: "t4g.small",
       desc: "Size of the swarm leader"
     });
     

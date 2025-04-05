7db451c5e69d5cfcf76dcbe91de9a59fcf9a705c
Olivier Pichon <op@dzango.com>
2025-04-05 21:35:38 +0200

    fix(aws): fix generator following name changes

--- generators/aws-s3/index.js
+++ generators/aws-s3/index.js
@@ -27,7 +27,7 @@ export default class AWSS3Generator extends PulumiGenerator {
 
 
     this.fs.copyTplAsync(
-      this.templatePath(this.name),
+      this.templatePath(`aws-${this.name}`),
       this.destinationPath(this._getFolderName()),
       {
         ...this.options,
@@ -44,7 +44,7 @@ export default class AWSS3Generator extends PulumiGenerator {
 
     if (this.options.createStackConfig) {
       this.fs.copyTplAsync(
-        `${this.templatePath(this.name)}/Pulumi.stack.yaml`,
+        `${this.templatePath(`aws-${this.name}`)}/Pulumi.stack.yaml`,
         `${this.destinationPath(this._getFolderName())}/Pulumi.${this.options.environment}.yaml`,
         {
           ...this.props,

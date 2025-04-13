8103847fcee119c530cbe018c346883d01b80283
Anthony-Phothirath <anthony@dzangolab.com>
2025-04-13 20:59:07 +0200

    Aws nfs server (#79)
    * chore: creating aws-nfs-server folder
    
    * chore: updating files
    
    * chore: fixing file
    
    * chore: updating
    
    * chore: adding changes
    
    * chore: syntax error
    
    * chore: update
    
    * chore: updated
    
    * chore: changed option props
    
    * Aws nf server 1 (#91)
    
    * feat: add support for versions (#81)
    
    * chore: release v1.0.0-rc18 (#82)
    
    * fix(PulumiGenerator): update template prompting code
    
    * chore: release v1.0.0-rc19 (#83)
    
    * fix(pulumi-s3-backend): fix project name in generated stack config (#84)
    
    * chore: release v1.0.0-rc20 (#85)
    
    * feat(pulumi-s3-backend): fix stack config template and docs (#86)
    
    * chore: release v1.0.0-rc21 (#87)
    
    * fix(pulumi-s3-backend): fix script style (#88)
    
    * chore: release v1.0.0-rc21.0 (#89)
    
    * fix(aws/ssh-keypairs): fix generator
    
    * chore: release v1.0.0-rc21.1 (#90)
    
    * fix(aws-nfs-server): minor fixes
    
    * fix(aws/nfs-server): update generator
    
    * fix(aws/nfs-server): remove size from stack config
    
    * fix(aws/nfs-server): fix generator
    
    ---------
    
    Co-authored-by: Anthony Phothirath <anthony.phothirath@hotmail.com>
    Co-authored-by: Olivier Pichon <203745+opichon@users.noreply.github.com>
    Co-authored-by: Olivier Pichon <op@dzango.com>

---
+++ generators/aws-nfs-server/index.js
@@ -0,0 +1,68 @@
+import chalk from "chalk";
+
+import PulumiGenerator from "../pulumi/index.js";
+
+export default class AWSNFSServerGenerator extends PulumiGenerator {
+  constructor(args, opts) {
+    super(args, opts);
+
+    this.displayName = "AWS NFS server";
+    this.name = "nfs-server";
+
+    this.option("availabilityZone", {
+      default: "ap-southeast-1a",
+      desc: "Available zone",
+      type: String,
+    });
+    
+    this.option("size", {
+      type: String,
+      default: "t4g.small",
+      desc: "Size of the NFS server"
+    });
+  }
+
+  async prompting() {
+    this.props = await this._optionOrPrompt([
+      {
+        default: this._getDefaultProjectName(),
+        message: "Enter the name of the pulumi project",
+        name: "projectName",
+        type: "input",
+      }
+    ]);
+  };
+
+  writing() {
+    const message = `Generating IaC code for ${this.displayName}`;
+    this.log(`${chalk.green(message)}`);
+
+
+    this.fs.copyTplAsync(
+      this.templatePath(`aws-${this.name}`),
+      this.destinationPath(this._getFolderName()),
+      {
+        ...this.options,
+        ...this.props,
+      },
+      {},
+      { 
+        globOptions: { 
+          dot: true,
+          ignore: ["**/Pulumi.stack.yaml"],
+        }
+      },
+    );
+
+    if (this.options.createStackConfig) {
+      this.fs.copyTplAsync(
+        `${this.templatePath(`aws-${this.name}`)}/Pulumi.stack.yaml`,
+        `${this.destinationPath(this._getFolderName())}/Pulumi.${this.options.environment}.yaml`,
+        {
+          ...this.options,
+          ...this.props,
+        }
+      );
+    }
+  };
+}

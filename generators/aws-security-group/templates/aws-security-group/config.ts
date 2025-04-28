6c21ca66188992539dca4df066d83a52e317e0b0
Anthony Phothirath <anthony.phothirath@hotmail.com>
2025-04-28 18:39:05 +0200

    fix: update getOutputs

--- generators/aws-security-group/templates/aws-security-group/config.ts
+++ generators/aws-security-group/templates/aws-security-group/config.ts
@@ -15,14 +15,11 @@ export const getConfig = async () => {
   let vpcId = stackConfig.get("vpcId");
 
   if (!vpcId) {
-    const vpcProject = stackConfig.get("vpcProject") || "aws-vpc";
-
-    const vpcStack = new StackReference(
-      `${organization}/${vpcProject}/${stack}`,
-    );
-
-    const vpcIdOutput = await vpcStack.getOutputDetails("vpcId");
-    vpcId = getValue<string>(vpcIdOutput);
+    const outputs = await getOutputs(
+      "vpcStack",
+      "vpcId"
+    )
+    vpcId = outputs ? outputs[0] as string : undefined;
   }
 
   return {
@@ -47,4 +44,84 @@ function getValue<T>(input: StackReferenceOutputDetails, defaultValue?: T): T {
   }
 
   return defaultValue;
+}
+
+const stacks: { [key: string]: StackReference } = {};
+
+async function getOutputs<T = string>(
+  stackConfigVar: string,
+  defaultOutputNames: string
+): Promise<undefined | T[]> {
+
+  const organization = getOrganization();
+  const stack = getStack();
+  const stackConfig = new Config();
+
+  const config = stackConfig.get(stackConfigVar);
+  if (!config) {
+    return undefined;
+  }
+
+  let [project, outputNamesString] = config.split(":");
+
+  if (!project) {
+    return undefined;
+  }
+
+  if (!outputNamesString) {
+    outputNamesString = defaultOutputNames;
+  }
+
+  if (!outputNamesString) {
+    return undefined;
+  }
+
+  const outputNames = outputNamesString.split(",");
+
+  let stackName = undefined;
+  let _organization = organization;
+  let _project = undefined;
+  let _stack = stack;
+
+  const tokens = project.split("/");
+
+  switch (tokens.length) {
+    case 3:
+      [_organization, _project, _stack] = tokens;
+      break;
+
+    case 2:
+      if (organization == "organization") {
+        [_project, _stack] = tokens;
+      } else {
+        [_organization, _project] = tokens;
+      }
+      break;
+
+    case 1:
+      _project = tokens[0];
+      break;
+  }
+
+  stackName = `${_organization}/${_project}/${_stack}`;
+  let otherStack = stacks[stackName];
+
+  if (!otherStack) {
+    otherStack = new StackReference(stackName);
+    stacks[stackName] = otherStack;
+  }
+
+  let outputs = [];
+
+  for (var i = 0, name = null; name = outputNames[i]; i++) {
+    const output = await otherStack.getOutputDetails(name);
+    if (output.value != undefined) {
+      outputs.push(getValue<T>(output) as T)
+    }
+    else {
+      outputs.push(undefined as unknown as T)
+    }
+  }
+
+  return outputs;
 }

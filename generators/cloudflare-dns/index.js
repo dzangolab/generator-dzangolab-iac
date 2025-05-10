65d5c19f8a6b7e7cb94103e9ce5aa6598f12dfac
Olivier Pichon <op@dzango.com>
2025-05-10 20:29:34 +0200

    fix(cloudflare): fix writing stack config

--- generators/cloudflare-dns/index.js
+++ generators/cloudflare-dns/index.js
@@ -55,7 +55,7 @@ export default class CloudflareDNSGenerator extends PulumiGenerator {
     );
     
     if (this.options.createStackConfig) {
-      this.fs.copyTplAsync(
+      await this.fs.copyTplAsync(
         `${this.templatePath(this.name)}/Pulumi.stack.yaml`,
         `${this.destinationPath(this._getFolderName())}/Pulumi.${this.options.environment}.yaml`,
         {
